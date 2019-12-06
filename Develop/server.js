e89d792f26652e4df954e2970e9e182db0f4c161
Pedja <vpedja12@gmail.com>
2019-12-05 17:39:36 -0800

    Completed post

--- Develop/server.js
+++ Develop/server.js
@@ -4,11 +4,12 @@ const fs = require("fs");
 const util = require("util");
 const jsonData = require("./db/db.json");
 
-const writefileAsync = util.promisify(fs.writeFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const noteArray = [];
+const noteArray;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -31,46 +32,32 @@ app.get('*', function (req, res) {
 });
 
 app.post("/api/notes", function (req, res) {
+
     res.send("Post received!");
 
     const newNote = req.body;
-    
-    // console.log(newNote);
-    noteArray.push(newNote);
-    console.log(noteArray);
 
-    readFileAsync()
-
-    // jsonfile.writeFileSync("./db/db.json", newNote, { spaces: 2, EOL: '\r\n'})
-
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            noteArray = JSON.parse(data);
+            if newNote.id || newNote.id === 0) {
+                let currentNote = noteArray[newNote.id];
+                currentNote.title = newNote.title;
+                currentNote.text = newNote.text;
+            } else {
+                noteArray.push(newNote);
+            }
+            writeFileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(noteArray))
+                .then(function () {
+                    console.log("Wrote note to JSON file.")''
+                })
+        });
+    res.json(newNote);
 });
 
 app.delete("/api/notes/:id", function (req, res) {
 
 });
-    // appendjson(newNote, "./db/db.json", function() {
-    //     console.log("Added to json file.")
-    // })
-    // newNote.save()
-    //     .then(item => {
-    //         res.send("item saved to database");
-    //     })
-    //     .catch(err => {
-    //         res.status(400).send("unable to save to database");
-    //     });
-// });
-
-    // // Using a RegEx Pattern to remove spaces from newCharacter
-    // // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-    // console.log(newCharacter);
-
-    // characters.push(newCharacter);
-
-    // res.json(newCharacter);
-// });
-
 
 
 app.listen(PORT, function () {
